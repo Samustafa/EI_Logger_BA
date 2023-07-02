5da1ca3d57a1aca86755da8030775f7b544fd45b
Hossam Al Mustafa <128149196+Samustafa@users.noreply.github.com>
2023-07-02 08:11:14 +0200

    EL-162: deleted unwanted extension artefacts and updated extension info

--- Extension/src/manifest.ts
+++ Extension/src/manifest.ts
@@ -6,9 +6,6 @@ const manifest: Manifest.WebExtensionManifest = {
     name: pkg.displayName,
     version: pkg.version,
     description: pkg.description,
-    options_ui: {
-        page: 'src/pages/options/index.html',
-    },
     background: {
         service_worker: 'src/pages/background/index.js',
         type: 'module',
@@ -16,9 +13,6 @@ const manifest: Manifest.WebExtensionManifest = {
     action: {
         default_popup: 'src/pages/popup/index.html',
         default_icon: 'icon-34.png',
-    },
-    chrome_url_overrides: {
-        newtab: 'src/pages/newtab/index.html',
     },
     icons: {
         '128': 'icon-128.png',
@@ -31,7 +25,6 @@ const manifest: Manifest.WebExtensionManifest = {
             css: ['contentStyle.css'],
         },
     ],
-    devtools_page: 'src/pages/devtools/index.html',
     web_accessible_resources: [
         {
             resources: ['contentStyle.css', 'icon-128.png', 'icon-34.png'],
