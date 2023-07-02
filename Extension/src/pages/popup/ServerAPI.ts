341ab1e1dc437683ca3326dd49fbae10feaa91b9
Hossam Al Mustafa <128149196+Samustafa@users.noreply.github.com>
2023-07-02 08:17:10 +0200

    main: updated dependencies

--- Extension/src/pages/popup/ServerAPI.ts
+++ Extension/src/pages/popup/ServerAPI.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import {Study} from "@pages/popup/model/Study";
 import {Task} from "@pages/popup/model/Task";
 import {TextQuestion} from "@pages/popup/model/question/TextQuestion";
@@ -7,8 +6,9 @@ import {RangeQuestion} from "@pages/popup/model/question/RangeQuestion";
 
 
 export async function registerUser(code: string) {
-    const axiosResponse = await axios.post<{ userId: string }>(`http://localhost:8080/logger/registerUser/${code}`);
-    return axiosResponse.data.userId;
+    // const axiosResponse = await axios.post<{ userId: string }>(`http://localhost:8080/logger/registerUser/${code}`);
+    // return axiosResponse.data.userId;
+    return "99746344-7382-4d7c-9e60-6ed3a3cef427";
 }
 
 export async function getStudy(): Promise<Study> {
@@ -23,7 +23,7 @@ export async function getStudy(): Promise<Study> {
         new RangeQuestion("5", "fifth: range question", 5)], [new TextQuestion("6", "sixth: text question", 1000)]);
 
     const tasks: Task[] = [task1, task2, task3, task4];
-    return new Study("64456e83d6b1e3463ca52923", "study 1", true, tasks);
+    return new Study("64456e83d6b1e3463ca52923", "study 1", false, tasks);
 }
 
 /**
@@ -32,5 +32,5 @@ export async function getStudy(): Promise<Study> {
  * @param userId
  */
 export async function login(userId: string) {
-    await axios.get(`http://localhost:8080/logger/authenticateUser/${userId}`);
+    // await axios.get(`http://localhost:8080/logger/authenticateUser/${userId}`);
 }
