56448aa235c12ca59b60751e156d0ccb3a5e156c
Hossam Al Mustafa <128149196+Samustafa@users.noreply.github.com>
2023-07-03 21:28:11 +0200

    main: mocked registration and login functions

--- Extension/src/pages/popup/ServerAPI.ts
+++ Extension/src/pages/popup/ServerAPI.ts
@@ -1,8 +1,8 @@
 import {Study} from "@pages/popup/model/Study";
 import {Task} from "@pages/popup/model/Task";
-import {TextQuestion} from "@pages/popup/model/question/TextQuestion";
-import {MultipleChoiceQuestion} from "@pages/popup/model/question/MultipleChoiceQuestion";
 import {RangeQuestion} from "@pages/popup/model/question/RangeQuestion";
+import {MultipleChoiceQuestion} from "@pages/popup/model/question/MultipleChoiceQuestion";
+import {TextQuestion} from "@pages/popup/model/question/TextQuestion";
 
 
 export async function registerUser(code: string) {
@@ -15,15 +15,26 @@ export async function getStudy(): Promise<Study> {
     // const axiosResponse = await axios.get<Study>(`http://localhost:8080/logger/getTestStudy`);
     // return axiosResponse.data;
 
-    const task1 = new Task("1", "Task 1", [], []);
-    const task2 = new Task("2", "Task 2", [new TextQuestion("1", "first: text question", 1000)], []);
-    const task3 = new Task("3", "Task 3", [], [new TextQuestion("2", "second: text question", 1000)]);
-    const task4 = new Task("4", "Task 4", [new TextQuestion("3", "third: text question", 1000),
-        new MultipleChoiceQuestion("4", "fourth: multiple choice question", ["choice 1", "choice 2", "choice 3"]),
-        new RangeQuestion("5", "fifth: range question", 5)], [new TextQuestion("6", "sixth: text question", 1000)]);
+    // const task1 = new Task("1", "Task 1", [], []);
+    // const task2 = new Task("2", "Task 2", [new TextQuestion("1", "first: text question", 1000)], []);
+    // const task3 = new Task("3", "Task 3", [], [new TextQuestion("2", "second: text question", 1000)]);
+    // const task4 = new Task("4", "Task 4", [new TextQuestion("3", "third: text question", 1000),
+    //     new MultipleChoiceQuestion("4", "fourth: multiple choice question", ["choice 1", "choice 2", "choice 3"]),
+    //     new RangeQuestion("5", "fifth: range question", 5)], [new TextQuestion("6", "sixth: text question", 1000)]);
+    //
+    // const tasks: Task[] = [task1, task2, task3, task4];
+    // const testStudy = new Study("64456e83d6b1e3463ca52923", "study 1", false, tasks);
 
-    const tasks: Task[] = [task1, task2, task3, task4];
-    return new Study("64456e83d6b1e3463ca52923", "study 1", false, tasks);
+    const penicillinTask = new Task("1", "When was Penicillin invented and by whom?", [], []);
+    // const penicillinStudy = new Study("1", "Penicillin", true, [penicillinTask]);
+
+    const johnsonTaskText = "Mr Johnson and his family are planning a trip to Paris. Please write a summary of the following: What cultural events are there (for example on August 2023)? What sightseeing to do? Please also cover hotels, flights, travelling to Paris, and weather.";
+    const johnsonTask = new Task("2", johnsonTaskText,
+        [new RangeQuestion("1", "From one to five, how fast, in your opinion, will you find the answer?", 10)],
+        [new MultipleChoiceQuestion("2", "After trying to use the internet to get your information, would you rather get that information from", ["The internet", "A social network like Facebook or Reddit where you post your question and expect people to answer", "Through a travelling agency"]), new TextQuestion("3", "What was the hardest part of finding your answer?", 1000)]);
+    const johnsonStudy = new Study("2", "Johnson", true, [penicillinTask, johnsonTask]);
+
+    return johnsonStudy;
 }
 
 /**
